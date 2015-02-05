7863d62386a85d10baeac799399fc62c2ef2c162
timchunght <timchunght@gmail.com>
2015-02-05 05:25:22 -0500

    finished project

--- PermutationTest.java
+++ PermutationTest.java
@@ -1,10 +1,9 @@
 import java.awt.*;
 import java.applet.*;
 import java.util.ArrayList;
-
 import graph.*;
 
-public class example1 extends Applet 
+public class PermutationTest extends Applet 
 {
 
       Graph2D graph;
@@ -20,13 +19,13 @@ public class example1 extends Applet
       double[] d1;
       double[] d2;
       double[] d3;
-      int MAXLENGTH = 16;
+      int MAXLENGTH = 17;
       boolean end = false;
 
       public void init() 
       {
-        //double x,y;
-
+       
+        setSize(750, 750);
         graph = new Graph2D();
         graph.drawzero = false;
         graph.drawgrid = false;
@@ -47,7 +46,6 @@ public class example1 extends Applet
 	        	 t.startTiming();
 	             alg1.generate(n);
 	             t.endTiming();
-	             //double elapsedTime = t.getElapsedTime();
 	             System.out.println("n: " + n);
 	             System.out.println("ElapsedTime: "+ t.getElapsedTime());
 	             dataAL.add((double) n);
@@ -91,7 +89,6 @@ public class example1 extends Applet
 	        	 t.startTiming();
 	             alg2.generate(n);
 	             t.endTiming();
-	             //double elapsedTime = t.getElapsedTime();
 	             System.out.println("n: " + n);
 	             System.out.println("ElapsedTime: "+ t.getElapsedTime());
 	             dataAL.add((double) n);
@@ -135,7 +132,6 @@ public class example1 extends Applet
 	        	 t.startTiming();
 	             alg3.generate(n);
 	             t.endTiming();
-	             //double elapsedTime = t.getElapsedTime();
 	             System.out.println("n: " + n);
 	             System.out.println("ElapsedTime: "+ t.getElapsedTime());
 	             dataAL.add((double) n);
@@ -168,68 +164,7 @@ public class example1 extends Applet
         
         d3 = convertALToArr(dataAL, d3);
         dataAL.clear();
-        
-        
-        
-        /**
-        d1 = new double[al.size()];
-        for(int k = 0; k < d1.length; k++ )
-        {
-        	d1[k] = al.get(k);
-        	System.out.println(al.get(k));
-        }
-        **/
-        /**
-        for(i = 0, n = 100; i < 2 * MAXPOINTS; i += 2, n *= 2) 
-        {
-            // If algorithm time is over 30 seconds, just set the next sets
-            // as 30 to keep y-axis limited to 30 seconds.
-          
-            if(end == false)
-            {
-           
-                t.startTiming();
-                alg1.generate(n);
-                t.endTiming();
-                //double elapsedTime = t.getElapsedTime();
-                System.out.println("n: " + n);
-                System.out.println("ElapsedTime: "+ t.getElapsedTime());
-                
-                // If time is over 30 seconds, set point as 30 seconds and
-                // flip boolean to save time.
-                if (t.getElapsedTime() > 30) 
-                {
-                    end = true;
-                    d1[i] = n;
-                    d1[i+1] = 30;
-                }
-                // Otherwise store data in array.
-                else
-                {
-                	System.out.println(i);
-                    d1[i] = n;
-                    d1[i+1] = t.getElapsedTime();
-                }
-            }
-            else
-            {
-            	d1[i] = d1[i-2];
-                d1[i+1] = d1[i-1];
-            }
-        }
-        **/
-        /**
-        t= new TimeInterval();
-        t.startTiming();
-        for(i=0; i<MAXPOINTS; i++){
-        	x=i;
-        	y=i*i;
-        	d2[i*2]=x; d2[i*2+1]=y;
-        }
-        t.endTiming();
-        System.out.println("time to generate " + MAXPOINTS + " values of Y=X*X is " + t.getElapsedTime()/1000.0);
-        **/
-        
+    
         data1 = graph.loadDataSet(d1, d1.length/2);
         data1.linestyle = 1;
         data1.linecolor   =  new Color(0,255,0);
@@ -259,8 +194,6 @@ public class example1 extends Applet
         data3.legendColor(Color.black);
 
         
-       
-        
         xaxis = graph.createAxis(Axis.BOTTOM);
         xaxis.attachDataSet(data1);
         xaxis.attachDataSet(data2);
@@ -268,10 +201,7 @@ public class example1 extends Applet
         xaxis.setTitleText("N number of values");
         xaxis.setTitleFont(new Font("TimesRoman",Font.PLAIN,20));
         xaxis.setLabelFont(new Font("Helvetica",Font.PLAIN,15));
-/*
-**      Attach the first data set to the Left Axis
-*/
-        
+    
         yaxis_left = graph.createAxis(Axis.LEFT);
         yaxis_left.attachDataSet(data1);
         yaxis_left.attachDataSet(data2);
